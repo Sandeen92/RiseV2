3ffbcc9b6dafd5ff2aaa6ab41234eb404972d4d2
Theodor Bäckstrand <theodor77@outlook.com>
2024-03-19 23:43:30 +0100

    1

---
+++ Program/src/Theodor_TestSuites/TestSuite_Dice.java
@@ -0,0 +1,19 @@
+package Theodor_TestSuites;
+
+import entity.Dice;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSuite_Dice {
+
+
+    @Before
+    public static void setUpDice(){
+
+        Dice dice = new Dice();
+
+    }
+
+    
+
+}
